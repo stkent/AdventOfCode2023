3ccd6450e59935517dc8502f4f44ce414b17d894
Stuart Kent <stuart@stkent.com>
2023-12-01 08:53:00 -0500

    Add day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,17 +2,61 @@ import utils.readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            line
+                .mapNotNull { char -> "$char".toIntOrNull() }
+                .let { 10 * it.first() + it.last() }
+        }
+    }
+
+    val digitWordMap = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9,
+    )
+
+    fun String.part2FindDigits(): List<Int> {
+        val result = mutableListOf<Int>()
+
+        // Iterate through this line from left to right, accumulating digits as we go:
+        for (index in indices) {
+            // Check for digit characters:
+            val char = get(index)
+            if (char.isDigit()) result.add("$char".toInt())
+
+            // Check for digit words:
+            for ((string, int) in digitWordMap) {
+                if (startsWith(string, startIndex = index)) {
+                    result.add(int)
+                    break
+                }
+            }
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            line
+                .part2FindDigits()
+                .let { 10 * it.first() + it.last() }
+        }
     }
 
-    val testInput = readInput("Day01_test.txt")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("Day01_test1")
+    check(part1(testInput1) == 142)
 
-    val input = readInput("Day01.txt")
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
+
+    val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
