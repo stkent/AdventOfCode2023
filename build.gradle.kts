f0a44e2b438dac308ff3d7344f8f8512f9373321
Stuart Kent <stuart@stkent.com>
2023-11-30 19:56:43 -0500

    Misc. project cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "1.9.21"
 }
 
 sourceSets {
     main {
         kotlin.srcDir("src")
-    }
-}
-
-tasks {
-    wrapper {
-        gradleVersion = "8.5"
     }
 }
--- src/Day01.kt
+++ src/Day01.kt
@@ -7,11 +7,10 @@ fun main() {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("Day01_test.txt")
     check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01.txt")
     part1(input).println()
     part2(input).println()
 }
