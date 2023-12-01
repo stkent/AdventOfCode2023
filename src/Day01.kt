c6c1e60568ac86fb0cc1a04ce8680cbe22c3bc2f
Stuart Kent <stuart@stkent.com>
2023-12-01 13:30:57 -0500

    Day 1 polish

--- src/Day01.kt
+++ src/Day01.kt
@@ -10,44 +10,38 @@ fun main() {
     }
 
     val digitWordMap = mapOf(
-        "one" to 1,
-        "two" to 2,
+        //@formatter:off
+        "one"   to 1,
+        "two"   to 2,
         "three" to 3,
-        "four" to 4,
-        "five" to 5,
-        "six" to 6,
+        "four"  to 4,
+        "five"  to 5,
+        "six"   to 6,
         "seven" to 7,
         "eight" to 8,
-        "nine" to 9,
+        "nine"  to 9,
+        //@formatter:on
     )
 
-    fun String.part2FindDigits(): List<Int> {
-        val result = mutableListOf<Int>()
+    fun String.toDigit(): Int {
+        return requireNotNull(this.toIntOrNull() ?: digitWordMap[this])
+    }
 
-        // Iterate through this line from left to right, accumulating digits as we go:
-        for (index in indices) {
-            // Check for digit characters:
-            val char = get(index)
-            if (char.isDigit()) result.add("$char".toInt())
-
-            // Check for digit words:
-            for ((string, int) in digitWordMap) {
-                if (startsWith(string, startIndex = index)) {
-                    result.add(int)
-                    break
-                }
-            }
+    fun String.part2LineNumber(): Int {
+        // List of all strings that represent digits ("1", "one", "2", "two", etc.):
+        val digitStrings = buildList {
+            addAll(digitWordMap.keys)
+            addAll(digitWordMap.values.map(Int::toString))
         }
 
-        return result
+        val (_, firstDigitString) = this.findAnyOf(digitStrings)!!
+        val (_, lastDigitString) = this.findLastAnyOf(digitStrings)!!
+
+        return 10 * firstDigitString.toDigit() + lastDigitString.toDigit()
     }
 
     fun part2(input: List<String>): Int {
-        return input.sumOf { line ->
-            line
-                .part2FindDigits()
-                .let { 10 * it.first() + it.last() }
-        }
+        return input.sumOf(String::part2LineNumber)
     }
 
     val testInput1 = readInput("Day01_test1")
